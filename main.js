c57b2da4aefdc5e7299c0651bed033f0cdd1d5d5
Redpug111 <75901583+Redpug111@users.noreply.github.com>
2021-10-22 13:04:25 +0000

    FIX

--- main.js
+++ main.js
@@ -1,112 +1,160 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 const config = require("./config.json");
 
 const fs = require('fs');
 
-var PouchDB = require('pouchdb');
-var db = new PouchDB('my_db');
-
 const ms = require('ms');
-const { count } = require('console');
 
-const jsonfile = require('jsonfile')
+client.once('ready', () => {
+  console.log('Bot activated! - Now detecting and compromising targets');
+  // Set the client user's presence
+  client.user.setPresence({
+    status: "dnd",
+    activity: {
+      name: 'Starting up...',
+      type: "COMPETING"
+    }
+  });
+  setInterval(function() {
+    var rand = Math.ceil(Math.random() * 5)
+    switch (rand) {
+      case 1:
+        client.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: 'you.',
+            type: "WATCHING"
+          }
+        });
+        break;
+      case 2:
+        client.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: 'Don\'t say it!',
+            type: "PLAYING"
+          }
+        });
+        break;
+      case 3:
+        client.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: 'I forgor 💀',
+            type: "PLAYING"
+          }
+        });
+        break;
+      case 4:
+        client.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: 'lmfao this nerd',
+            type: "WATCHING"
+          }
+        });
+        break;
+      case 5:
+        client.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: 'you hopefully not saying the word.',
+            type: "LISTENING"
+          }
+        });
+        break;
+      case 6:
+        client.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: 'I rember 😇',
+            type: "PLAYING"
+          }
+        });
+        break;
+      default:
+        console.log("help")
+    }
+    //debug
+    //console.log("status change: " + rand);
+  }, 15000);
 
-client.commands = new Discord.Collection();
- 
-client.once('ready', () =>{
-    console.log('Bot activated!');
-     const guildNames = client.guilds.cache.map(g => g.id)
-     for (g = 0; g < guildNames.length; g++) {
-        // lmaoarray = {"list":["lmao", "iamo", "breh"]}
-
-        // var file = `./lmaoarrays/guild_${guildNames[g].toString()}.json`
-
-        // var obj = JSON.stringify(lmaoarray)
-
-        // jsonfile.writeFileSync(file, obj)
-        
-        // results = JSON.parse(jsonfile.readFileSync(`./lmaoarrays/guild_${guildNames[g].toString()}.json`))     
-        // console.log(results)
-     }
 });
 
+var untilSecret = Math.ceil(Math.random() * 4) + 3
+console.log("Until Secret: " + untilSecret)
+client.on('message', message => {
+  var words = message.content.toString().toLowerCase();
+  words = words.replace(/ /g, "")
+  words = words.replace(/\*/g, "")
+  words = words.replace(/_/g, "")
 
-client.on('message', message =>{
-    i = 0
-    var words = message.content.toString().toLowerCase();
-    words = words.replace(/ /g, "")
-    words = words.replace(/\*/g, "")
-    words = words.replace(/_/g, "")
-    var file = `./lmaoarrays/guild_${message.guild.id.toString()}.json`
-    fs.access(file, fs.F_OK, (err) => {
-        if (err) {
-            lmaodefault = ["iamo","lmao"]
-            jsonfile.writeFileSync(file, JSON.stringify(lmaodefault))
+
+  //HA HERE IT IS NERDS
+  blacklist = ["lmao", "imao", "lmfao", "imfao", "lma0", "ima0"];
+  //RIGHT HERE LMFAO
+
+  if (message.content.startsWith("blamo;say ")) {
+    if (message.author.id !== config.ownerID && message.author.id !== "805464078364573706") {
+      console.log("Failed blamosay attempt: " + message.author.username);
+      return;
+    }
+    args = message.content.split(" ");
+    args = args.slice(1, args.length);
+    msg = args.slice(1, args.length).join(" ")
+    msgchnl = client.channels.cache.get(args[0]);
+    if (msgchnl) {
+    msgchnl.send(msg)
+    console.log("#" + msgchnl.name + " | Blamo: " + msg)
+    }
+    else{
+      message.channel.send("invalid id!")
+      console.log("invalid channel id")
+    }
+    
+  }
+  for (var i = 0; i < blacklist.length; i++) {
+    if (words.includes(blacklist[i])) {
+      untilSecret--;
+      console.log("#" + message.channel.name + ": " + message.author.username + "#" + message.author.discriminator + ": " + message.content);
+      var special = parseInt(Math.ceil(Math.random() * 100))
+      console.log(special)
+      if (special <= 5) {
+        setTimeout(function() { message.channel.send("I forgor to say blamo."); setTimeout(function() { message.channel.send("💀") }, 1000) }, 5000)
+        console.log("Woah, lucky!")
+      }
+      else if (special > 5) {
+        if(untilSecret == 0){
+          untilSecret = Math.ceil(Math.random() * 3) + 4
+          console.log("secret given: " + "temp")
+          message.channel.send("Blamo!" + " test, dont pay attention to this")
         }
-        console.log(`file exists`)
-        //file exists
-      })
-    lmaos = JSON.parse(jsonfile.readFileSync(`./lmaoarrays/guild_${message.guild.id.toString()}.json`)).list
-    lmaosRaw = JSON.parse(jsonfile.readFileSync(`./lmaoarrays/guild_${message.guild.id.toString()}.json`))
-    
-    if (message.author.id == "804841786807287829") return;
-    else if (message.channel.type === 'dm'){
-        console.log(`**${message.author.username}:** ${message.content}`)
-    }
-    else if (words.includes("!lmaoadd") && (message.author.id == config.ownerID || message.author.id == config['1ID'] || message.author.id == config['2ID'])){
-        var arg = message.content
-        arg = arg.split(' ').slice(1).join(' ')
-        console.log(arg + " was added to array")
-        console.log(lmaos)
-        lmaosRaw.list.push(JSON.stringify(arg))
-        console.log(lmaosRaw)
-        console.log(lmaosRaw.list)
-        //jsonfile.writeFileSync(file, writefile)
-        message.reply(`Success! ${arg} was added to the list!`)
-        console.log("------------------------")
-        console.log(lmaos)
-        console.log(lmaosRaw)
-        console.log(lmaosRaw.list)
-    }
-    else if (words.includes("!lmaolist") && (message.author.id == config.ownerID || message.author.id == config['1ID'] || message.author.id == config['2ID'])){
-        message.channel.send("Here is the array of lmaos: \n"+ lmaos)
-        console.log(lmaos)
-        console.log(lmaosRaw.list)
-    }
-    
-    else if (words.includes("!lmaoreset") && (message.author.id == config.ownerID || message.author.id == config['1ID'] || message.author.id == config['2ID'])){
-        lmaos = ["lmao", "iamo"]
-        message.channel.send("reset array.")
-        console.log(lmaos)
-    }
-    else if (words.includes("!lmaoremove") && (message.author.id == config.ownerID || message.author.id == config['1ID'] || message.author.id == config['2ID'])){
-        var arg = message.content
-        arg = arg.split(' ').slice(1).join(' ')
-        console.log(arg + " was removed from array")
-        lmaos = lmaos.filter(v => v !== arg); 
-        message.reply(`Success! ${arg} was removed from the list!`)
-        console.log(lmaos)
-    }
-    else if (words.includes("lmaow") || words.includes("lmonke")){
-        message.reply("congratz you are epic")
-        console.log("someones epic")
-    }
-    else for (i in lmaosRaw.list[i]){
-        if (words.includes(lmaosRaw.list[i])){
-            message.channel.send("Blamo!")
-            i++
+        else{
+          message.channel.send("Blamo!");
         }
+      }
+      else {
+        console.log("error: luck system")
+      }
     }
-    
+  }
+
+
 });
 
-client.on("guildMemberUpdate", function(oldMember, newMember){
-    if (oldMember.user.id == "804841786807287829"){
-        newMember.setNickname("Blamo")
+client.on("messageDelete", function(message) {
+    if (message.author.id == client.user.id) {
+      console.log("Message deleted in #" + message.channel.name + ": " + message.content)
     }
+});
+
+client.on("guildMemberUpdate", function(oldMember, newMember) {
+  if (oldMember.user.id == "804841786807287829") {
+    newMember.setNickname("Blamo")
+  }
 });
 
 client.login(config.token);
